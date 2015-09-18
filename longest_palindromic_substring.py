8166acf48f47e3d1d5f26251b2b800c34f8ea723
Theresa Cay <cay.theresa@gmail.com>
2015-09-17 19:11:16 -0700

    Completed dynamic programming solution with testing

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -1,6 +1,8 @@
 """
 Longest palindromic substring
 	--> Given a string, find the longest substring which is a palindrome
+
+http://www.geeksforgeeks.org/longest-palindromic-substring-set-2/
 """
 
 # Brute force solution
@@ -32,6 +34,52 @@ def longest_palindromic_substring(string):
 				max_length = j+1-i
 
 	return max_palindrome
+
+#########################################################################################
+
+# Dynamic programming solution
+# Time: O(n^2)
+def dynamic_LPS(string):
+	"""
+	>>> longest_palindromic_substring("forgeeksskeegfor")
+	'geeksskeeg'
+
+	>>> longest_palindromic_substring("l")
+	'l'
+
+	>>> longest_palindromic_substring("no palindrome")
+	''
+	"""
+
+	max_length = 1
+
+	start = 0
+
+	low = 0
+	high = 0
+
+	for i in range(1, len(string)):
+		low = i - 1
+		high = i
+
+		while low >= 0 and high < len(string) and string[low] == string[high]:
+			if high-low+1 > max_length:
+				start = low
+				max_length = high - low + 1
+			low -= 1
+			high += 1
+
+		low = i-1
+		high = i + 1
+
+		while low >= 0 and high < len(string) and string[low] == string[high]:
+			if high-low+1 > max_length:
+				start = low
+				max_length = high - low + 1
+			low -= 1
+			high += 1
+
+	print string[low:high+1]
 
 #########################################################################################
 
