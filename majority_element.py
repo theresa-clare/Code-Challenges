595acf7d7d68e83222f6102a693c0d96a92d84c4
Theresa Cay <cay.theresa@gmail.com>
2015-09-18 17:49:18 -0700

    Completed alternative functions using Moore's Voting Algorithm with time/space complexity and testing

--- majority_element.py
+++ majority_element.py
@@ -4,6 +4,8 @@ A majority element in an array A[] of size n is an element that appears more tha
 
 Write a funciton which takes an array and emits the majority element (if it exists),
 otherwise returns None
+
+http://www.geeksforgeeks.org/majority-element/
 """
 
 # Time: O(n)
@@ -27,6 +29,75 @@ def majority_element(num_list):
 
 	return None
 
+#########################################################################################
+
+# Using Moore's Voting Algorithm
+# Overall Time: O(n)
+# Overall Space: O(1)
+
+def find_candidate(num_list):
+	"""
+	Given a list, returns an element occuring most of the time in the array
+
+	>>> find_candidate([2, 2, 3, 5, 2, 2, 6])
+	2
+
+	>>> find_candidate([])
+
+	"""
+
+	if num_list is None or len(num_list) == 0:
+		return None
+
+	majority_index = 0
+	count = 1
+
+	for i in range(1, len(num_list)):
+		if num_list[majority_index] == num_list[i]:
+			count += 1
+		else:
+			count -= 1
+
+		if count == 0:
+			majority_index = i
+			count = 1
+
+	return num_list[majority_index]
+
+def is_majority(num_list, candidate):
+	"""
+	Given a list and a candidate, returns True if candidate is the majority element;
+	returns False otherwise
+
+	>>> is_majority([1, 1, 1, 7, 2, 1], 1)
+	True
+
+	>>> is_majority([1, 1, 1, 7, 2, 2], 1)
+	False
+	"""
+	count = 0
+
+	for num in num_list:
+		if num == candidate:
+			count += 1
+
+	return count > len(num_list)/2
+
+def find_majority_element(num_list):
+	"""
+	Given a list, returns the majority element if one exists
+
+	>>> find_majority_element([3, 3, 4, 2, 4, 4, 2, 4, 4])
+	4
+
+	>>> find_majority_element([3, 3, 4, 2, 4, 4, 2, 4])
+
+	"""
+	candidate = find_candidate(num_list)
+
+	if is_majority(num_list, candidate):
+		return candidate
+	return None
 
 #########################################################################################
 
