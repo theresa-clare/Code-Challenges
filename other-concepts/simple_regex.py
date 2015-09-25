0ccd172a0f8b8d21936aa8765d55776bc58a30c4
Theresa Cay <cay.theresa@gmail.com>
2015-09-24 18:19:52 -0700

    Placed files into folders based on category

---
+++ other-concepts/simple_regex.py
@@ -0,0 +1,80 @@
+"""
+Given a regular expression pattern and a string, check to see if the pattern matches the string
+
+* --> 0 or more
+. --> anything and has to be present
+"""
+
+def tokenize(pattern):
+	""" 
+	Returns a list of the pattern tokenized
+
+	>>> tokenize("c*b*c.")
+	['c*', 'b*', 'c', '.']
+
+	>>> tokenize("cc")
+	['c', 'c']
+
+	>>> tokenize("")
+	[]
+	"""
+	if pattern is None:
+		return None
+	elif len(pattern) == 0:
+		return []
+
+	res = []
+	i = 0
+
+	while i < len(pattern):
+		if i+1 < len(pattern) and pattern[i+1] == '*':
+			res.append(pattern[i:i+2])
+			i += 2
+		else:
+			res.append(pattern[i])
+			i += 1
+
+	return res
+
+def match_tokenized_regex_pattern(pattern, string):
+	"""
+	Given a tokenized list of a regex pattern and a string, returns True if the 
+	pattern matches the string; returns False otherwise
+
+	>>> match_tokenized_regex_pattern(['c*', 'b*', 'c', '.'], "ccbcd")
+	True
+
+	>>> match_tokenized_regex_pattern(['c', 'b', '.'], "cbd")
+	True
+
+	>>> match_tokenized_regex_pattern(['c', 'c'], "abc")
+	False
+	"""
+	if not pattern and not string: # if reach the end of both pattern and string
+		return True
+	if not pattern or not string: # if reach the end of either pattern and string
+		return False
+
+	p = pattern[0]
+	c = string[0]
+
+	# removes cases with (letter-symbol vs. letter) and (non-matching single letters)
+	if p == c or p == '.':
+		return match_tokenized_regex_pattern(pattern[1:], string[1:])
+
+	# X* --> X can appear 0 times, once, or more (3 cases)
+	elif len(pattern) > 1:
+		if pattern[0] == c:
+			is_match = match_tokenized_regex_pattern(pattern, string[1:]) # appear >= 1 times
+			if is_match:
+				return True
+		return match_tokenized_regex_pattern(pattern[1:], string) # appear 0 times in string
+
+	# non-matching single letters
+	else:
+		return False
+#########################################################################################
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
