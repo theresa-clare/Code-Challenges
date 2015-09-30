e36a13eb8af3459b75bc2a5ddc2f1e39114bbe0c
Theresa Cay <cay.theresa@gmail.com>
2015-09-29 21:39:17 -0700

    Completed second solution using recursion and dynamic programming/memoization with testing

--- other-concepts/longest_word.py
+++ other-concepts/longest_word.py
@@ -35,6 +35,53 @@ def longest_word(words):
 
 #########################################################################################
 
+# Recursively see if can build right side from other elements in array to account for
+# composites of more than one word (uses dynamic programming and memoization)
+
+def get_longest_word(words):
+	"""
+	Returns longest word made of other words in the list
+
+	>>> get_longest_word(["cat", "banana", "dog", "nana", "walk", "walker", "dogwalker"])
+	'dogwalker'
+
+	>>> get_longest_word(["apple", "berry", "cherry", "appleberrycherry", "durian", "berrycherry"])
+	'appleberrycherry'
+
+	>>> get_longest_word(["cat", "dog", "squirrel", "seahorse"])
+
+	>>> get_longest_word([])
+
+	"""
+	words.sort(key=len, reverse=True)
+	word_dict = {}
+
+	for word in words:
+		word_dict[word] = True
+
+	for w in words:
+		if can_build_word(w, True, word_dict):
+			return w
+
+	return None
+
+def can_build_word(string, is_original, word_dict):
+	if string in word_dict and not is_original:
+		return True
+
+	for i in range(1, len(string)):
+		left = string[:i]
+		right = string[i:]
+
+		if left in word_dict and word_dict[left] == True and \
+		can_build_word(right, False, word_dict):
+			return True
+
+	word_dict[string] = False
+	return False
+
+#########################################################################################
+
 if __name__ == "__main__":
 	import doctest
 	doctest.testmod()
