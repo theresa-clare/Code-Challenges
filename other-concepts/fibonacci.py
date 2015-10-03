8ae4ff1c26a0e392e39d72c413eec2d62f079633
Theresa Cay <cay.theresa@gmail.com>
2015-10-02 18:47:07 -0700

    Completed function iteratively without memoization

--- other-concepts/fibonacci.py
+++ other-concepts/fibonacci.py
@@ -50,6 +50,22 @@ def bottom_up_fibonacci(n):
 
 #########################################################################################
 
+def fibonacci_v2(n):
+	if n == 0:
+		return 0
+
+	a = 0
+	b = 1
+
+	for i in range(2, n):
+		c = a + b
+		a = b
+		b = a
+
+	return a + b
+
+#########################################################################################
+
 if __name__ == "__main__":
 	import doctest
 	doctest.testmod()
