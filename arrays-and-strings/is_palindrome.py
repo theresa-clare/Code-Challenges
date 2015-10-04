39d09e3a7c7de028feee66b218ab11dbd6f44f7a
Theresa Cay <cay.theresa@gmail.com>
2015-10-04 02:08:38 -0700

    Completed optimized function to account for inputs with spaces and punctuation with time/space complexity and testing

--- arrays-and-strings/is_palindrome.py
+++ arrays-and-strings/is_palindrome.py
@@ -19,6 +19,36 @@ def is_palindrome(word):
 
 	return True
 
+#########################################################################################
+
+# Takes into account if input has spaces, punctuation, etc
+# Time: O(n)
+# Space: O(1)
+def is_palindrome_optimized(candidate):
+	"""
+	Returns True if input string is a palindrome
+
+	>>> is_palindrome_optimized('a b , c, cba')
+	True
+
+	>>> is_palindrome_optimized('abcdcba')
+	True
+	"""
+	start = 0
+	end = len(candidate)-1
+
+	while start != end:
+		if not candidate[start].isalpha():
+			start += 1
+		elif not candidate[end].isalpha():
+			end -= 1
+		else:
+			if candidate[start] != candidate[end]:
+				return False
+			start += 1
+			end -= 1
+            
+	return True
 
 #########################################################################################
 
