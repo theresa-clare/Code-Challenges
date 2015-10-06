385cdcac5d8b08a183d7f64b55610f8f3ee44eeb
Theresa Cay <cay.theresa@gmail.com>
2015-10-06 11:16:08 -0700

    Completed function with testing

---
+++ arrays-and-strings/anagram_buckets.py
@@ -0,0 +1,31 @@
+def anagram_buckets(words):
+	"""
+	Given an array of words, print all anagrams together
+
+	>>> anagram_buckets(['cat', 'atc', 'banana', 'cool', 'loco'])
+	[['banana'], ['cool', 'loco'], ['cat', 'atc']]
+
+	>>> anagram_buckets(['python', 'thopny', 'ythonp'])
+	[['python', 'thopny', 'ythonp']]
+	"""
+	word_dict = {}
+
+	for word in words:
+		sorted_word = "".join(sorted(word))
+		if sorted_word in word_dict:
+			word_dict[sorted_word].append(word)
+		else:
+			word_dict[sorted_word] = [word]
+
+	res = []
+
+	for sorted_w, w in word_dict.iteritems():
+		res.append(w)
+
+	return res
+
+#########################################################################################
+
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
