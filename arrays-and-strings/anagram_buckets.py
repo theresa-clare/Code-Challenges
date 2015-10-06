d8e7b464f673bcf2c5caee3083449bbc6a6b35f6
Theresa Cay <cay.theresa@gmail.com>
2015-10-06 11:23:27 -0700

    Completed function using dictionary comprehension for initializing dictionary with testing

--- arrays-and-strings/anagram_buckets.py
+++ arrays-and-strings/anagram_buckets.py
@@ -26,6 +26,25 @@ def anagram_buckets(words):
 
 #########################################################################################
 
+def anagram_buckets_v2(words):
+	"""
+	Given an array of words, print all anagrams together
+
+	>>> anagram_buckets_v2(['cat', 'atc', 'banana', 'cool', 'loco'])
+	[['banana'], ['cool', 'loco'], ['cat', 'atc']]
+
+	>>> anagram_buckets_v2(['python', 'thopny', 'ythonp'])
+	[['python', 'thopny', 'ythonp']]
+	"""
+	word_dict = {"".join(sorted(word)): [] for word in words}
+
+	for w in words:
+		word_dict["".join(sorted(w))].append(w)
+
+	return word_dict.values()
+
+#########################################################################################
+
 if __name__ == "__main__":
 	import doctest
 	doctest.testmod()
