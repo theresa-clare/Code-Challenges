453b938221fdef78dc809fb6d4561fd5b4ee9cb2
Theresa Cay <cay.theresa@gmail.com>
2015-10-14 12:27:03 -0500

    Completed division function with testing

--- other-concepts/implement_operations.py
+++ other-concepts/implement_operations.py
@@ -47,6 +47,36 @@ def multiply(a, b):
 
 #########################################################################################
 
+def divide(a, b): # a = bx
+	"""
+	Given two numbers, returns the quotient using only the add operator.
+
+	>>> divide(10, 5)
+	2
+
+	>>> divide(3, 0)
+	'Error. Cannot divide by 0'
+	"""
+	if b == 0:
+		return 'Error. Cannot divide by 0'
+
+	abs_a = abs(a)
+	abs_b = abs(b)
+	product = 0
+	quotient = 0
+
+	while product + abs_b <= abs_a:
+		product += abs_b
+		quotient += 1
+
+	if (a < 0 and b < 0) or (a > 0 and b > 0):
+		return quotient
+	else:
+		return -quotient
+	
+
+#########################################################################################
+
 if __name__ == '__main__':
 	import doctest
 	doctest.testmod()
