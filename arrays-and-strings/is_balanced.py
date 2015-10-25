84b5a4ccd7661cfc8baee1ecde6eb37d19c6acfb
Theresa Cay <cay.theresa@gmail.com>
2015-10-25 11:43:00 -0700

    Completed main and helper function with testing

---
+++ arrays-and-strings/is_balanced.py
@@ -0,0 +1,66 @@
+"""
+Given an expression from a string, write a program to examine whether the pairs
+and the orders are correct in the expression. Return True if in right order;
+return False otherwise
+"""
+
+def is_parens(char):
+	"""
+	Returns True if character is a type of parentheses; returns False otherwise
+
+	>>> is_parens("{")
+	True
+
+	>>> is_parens("c")
+	False
+
+	>>> is_parens("char")
+	False
+	"""
+	parens = {"(", ")", "[", "]", "{", "}"}
+
+	if char in parens:
+		return True
+	return False
+
+def is_balanced(parens):
+	"""
+	Given an expression, returns True if the parentheses are balanced
+
+	>>> is_balanced("{}[()]")
+	True
+
+	>>> is_balanced("([)[]")
+	False
+
+	>>> is_balanced("(c)")
+	True
+	"""
+	matching_parens = {
+		")": "(",
+		"]": "[",
+		"}": "{"
+	}
+	parens_stack = []
+
+	for paren in parens:
+		if not is_parens(paren):
+			continue
+		
+		if paren not in matching_parens: # if opening parentheses
+			parens_stack.append(paren)
+		else:
+			if parens_stack[-1] != matching_parens[paren]:
+				return False
+			else:
+				parens_stack.pop()
+
+	if parens_stack:
+		return False
+	return True
+
+#################################################################################
+
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
